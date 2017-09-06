4be23e867e9ea51fc68768418b055c556f506cdf
Michael Dunlap <sagaquisces@gmail.com>
2017-09-05 20:08:33 -0700

    add ComposersActivity and create Intent from MainActivity

---
+++ app/src/main/java/com/epicodus/symphoniccomposers/ComposersActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.symphoniccomposers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComposersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_composers);
+    }
+}
--- app/src/main/java/com/epicodus/symphoniccomposers/MainActivity.java
+++ app/src/main/java/com/epicodus/symphoniccomposers/MainActivity.java
@@ -1,10 +1,10 @@
 package com.epicodus.symphoniccomposers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindComposersButton;
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
         mFindComposersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, ComposersActivity.class);
+                startActivity(intent);
             }
         });
     }
