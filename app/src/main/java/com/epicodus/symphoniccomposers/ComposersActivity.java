23f6037d4fd690a8fc2bdbfdc31e205e13c6c712
Michael Dunlap <sagaquisces@gmail.com>
2017-09-05 21:16:04 -0700

    pass data from main activity to composers activity

--- app/src/main/java/com/epicodus/symphoniccomposers/ComposersActivity.java
+++ app/src/main/java/com/epicodus/symphoniccomposers/ComposersActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.symphoniccomposers;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ComposersActivity extends AppCompatActivity {
+    private TextView mCountryTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composers);
+        mCountryTextView = (TextView) findViewById(R.id.countryTextView);
+        Intent intent = getIntent();
+        String country = intent.getStringExtra("country");
+        mCountryTextView.setText("Here are all the composers from: " + country);
     }
 }
--- app/src/main/java/com/epicodus/symphoniccomposers/MainActivity.java
+++ app/src/main/java/com/epicodus/symphoniccomposers/MainActivity.java
@@ -5,20 +5,26 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindComposersButton;
+    private EditText mCountryEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mCountryEditText = (EditText) findViewById(R.id.countryEditText);
         mFindComposersButton = (Button) findViewById(R.id.findComposersButton);
         mFindComposersButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String country = mCountryEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, ComposersActivity.class);
+                intent.putExtra("country", country);
                 startActivity(intent);
             }
         });
