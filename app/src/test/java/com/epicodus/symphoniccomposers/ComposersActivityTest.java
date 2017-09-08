103da276635f49cf10b8acf7cfbdfbb58e07b89c
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-08 10:22:31 -0700

    ComposersActivity listView test for successful population of list view passes

---
+++ app/src/test/java/com/epicodus/symphoniccomposers/ComposersActivityTest.java
@@ -0,0 +1,38 @@
+package com.epicodus.symphoniccomposers;
+
+import android.os.Build;
+import android.widget.ListView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Guest on 9/8/17.
+ */
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class ComposersActivityTest {
+    private ComposersActivity activity;
+    private ListView mComposerListView;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(ComposersActivity.class);
+        mComposerListView = (ListView) activity.findViewById(R.id.listView);
+    }
+
+    @Test
+    public void composerListViewPopulates() {
+        assertNotNull(mComposerListView.getAdapter());
+        assertEquals(mComposerListView.getAdapter().getCount(), 12);
+    }
+}
