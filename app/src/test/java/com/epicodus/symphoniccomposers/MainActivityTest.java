8609721fcfa61ee73ca93ab7fcbe5a2f031d9565
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-08 08:48:28 -0700

    pass two test for MainActivity: text is validated for TextView and the button to click to the Composers Activity works

---
+++ app/src/test/java/com/epicodus/symphoniccomposers/MainActivityTest.java
@@ -0,0 +1,46 @@
+package com.epicodus.symphoniccomposers;
+
+import android.content.Intent;
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Guest on 9/8/17.
+ */
+
+@Config(constants=BuildConfig.class, sdk= Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView appNameTextView = (TextView) activity.findViewById(R.id.appNameTextView);
+        assertTrue("Symphonic Composers".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted() {
+        activity.findViewById(R.id.findComposersButton).performClick();
+        Intent expectedIntent = new Intent(activity, ComposersActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
+}
