dd63d733cb3a74952d9773c8ba41cafedfb0224b
Michael Dunlap <michaeldunlap@earthlink.net>
2017-09-08 08:57:53 -0700

    pass instrumentation test that enters a country into the country editText field

---
+++ app/src/androidTest/java/com/epicodus/symphoniccomposers/MainActivityInstrumentationTest.java
@@ -0,0 +1,29 @@
+package com.epicodus.symphoniccomposers;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Guest on 9/8/17.
+ */
+
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.countryEditText)).perform(typeText("Germany"))
+                .check(matches(withText("Germany")));
+    }
+}
